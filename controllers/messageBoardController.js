439ff3753e0628cca612f1c37aed8411d42a15de
elaaasri <anas_elasri@outlook.fr>
2026-09-11 17:22:10 +0100

    style: add message details styling

--- controllers/messageBoardController.js
+++ controllers/messageBoardController.js
@@ -14,7 +14,7 @@ const messages = [
 ];
 
 const getIndex = (req, res) => {
-  res.render("index", { title: "Mini Message Board", messages });
+  res.render("index", { messages });
 };
 
 const handleMessageLike = (req, res) => {
